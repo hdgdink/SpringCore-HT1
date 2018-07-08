3717e9543815eb059661dc2a69908f5a06bc75a9
hdgdink <hdgdink@list.ru>
2018-07-08 21:37:21 +0600

    code refactoring

--- src/main/java/kz/epam/spring/hometask1/runner/App.java
+++ src/main/java/kz/epam/spring/hometask1/runner/App.java
@@ -6,12 +6,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("ua.epam.spring.hometask")
+@ComponentScan("kz.epam.spring.hometask1")
 public class App {
     private static ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
     private static DataInitializer dataInitializer;
     private static MenuServiceImpl menuService;
-
 
     public App(DataInitializer dataInitializer, MenuServiceImpl menuService) {
         App.dataInitializer = dataInitializer;
